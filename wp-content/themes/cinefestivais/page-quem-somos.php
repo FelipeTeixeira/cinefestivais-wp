8a77b5f2823eabc057713b77785033fceaf098b4
Felipe <felipe.teixeira@gsw.com.br>
2019-09-07 10:10:20 -0300

    pagina qem somos

--- wp-content/themes/cinefestivais/page-quem-somos.php
+++ wp-content/themes/cinefestivais/page-quem-somos.php
@@ -14,12 +14,12 @@
 <header class="post-header full bg-img bg-quemsomos">
 </header>
 
-<article>
-    <section class="content content-card">
+<article class="content">
+    <section class="content-card">
         <h1 class="content-card--title">
 			Quem somos
 		</h1>
-		<article class="categoryText article-text">
+		<article class="content-card-text">
 			<?php the_content(); ?>
 		</article>
 	</section>
