bbf33edeae51ebc82dfdbc53cddbab3d411d9aeb
Felipe <felipe.teixeira@gsw.com.br>
2019-09-08 21:40:01 -0300

    remcao de comentarios

--- wp-content/themes/cinefestivais/single.php
+++ wp-content/themes/cinefestivais/single.php
@@ -175,7 +175,7 @@ if (have_posts()) : while (have_posts()) : the_post();
 
 	</section>
 		
-	<button type="button" class="singlePg-btnComments btn btn-primary">
+	<button type="button" class="singlePg-btnComments btn btn-primary  ">
 		<svg class="icon icon-chevron-down">
 			<use xlink:href="#icon-chevron-down"></use>
 		</svg>
