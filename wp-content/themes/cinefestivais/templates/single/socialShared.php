0036bb842d9eea0d053073bc6efcbdb5648877d3
Felipe <felipe.teixeira@gsw.com.br>
2019-09-13 14:14:33 -0300

    social shared

--- wp-content/themes/cinefestivais/templates/single/socialShared.php
+++ wp-content/themes/cinefestivais/templates/single/socialShared.php
@@ -9,26 +9,33 @@
         </li>
 
         <li class="socialShared-divisor">
-            <a href="" class="socialShared-item socialShared-item--shared">
+            <span class="socialShared-item socialShared-item--shared">
                 <svg class="icon icon-share-alt">
                     <use xlink:href="#icon-share-alt"></use>
                 </svg>
                 <span class="socialShared-item--shared-text">
                     COMPARTILHE
                 </span>
-            </a>
+            </span>
         </li>
 
         <li>
-            <a href="" class="socialShared-item socialShared-item-circle">
+            <a class="socialShared-item socialShared-item-circle" aria-label="Compartilhe no Facebook" href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink()?>" onclick="window.open(this.href, 'facebook-share','width=580,height=296');return false;" title="Compartilhe no Facebook">    
                 <svg class="icon icon-facebook">
                     <use xlink:href="#icon-facebook"></use>
                 </svg>
             </a>
         </li>
 
+        <?php 
+            $content = get_the_excerpt();
+            $conteudo = substr($content, 0, 70) . '...';                        
+        ?>
         <li>
-            <a href="" class="socialShared-item socialShared-item-circle">
+        <a class="socialShared-item socialShared-item-circle" aria-label="Compartilhe no Twitter" 
+            href="https://twitter.com/intent/tweet?url=<?php the_permalink()?>&amp;text=<?php echo $conteudo; ?>&amp;via=artigo19" 
+            onclick="window.open(this.href, 'twitter-share', 'width=550,height=235');return false;" 
+            title="Compartilhe no Twitter">
                 <svg class="icon icon-twitter">
                     <use xlink:href="#icon-twitter"></use>
                 </svg>
@@ -36,7 +43,7 @@
         </li>
 
         <li>
-            <a href="" class="socialShared-item socialShared-item-circle">
+            <a href="mailto:?subject=Cinefestivais&amp;body=<?php the_title(); echo " "; the_permalink()?>" target="_top" class="socialShared-item socialShared-item-circle">
                 <svg class="icon icon-envelope">
                     <use xlink:href="#icon-envelope"></use>
                 </svg>
@@ -44,7 +51,7 @@
         </li>
         
         <li>
-            <a href="" class="socialShared-item socialShared-item-circle">
+            <a class="socialShared-item socialShared-item-circle" href="https://wa.me/?text=<?php the_title(); echo " "; the_permalink()?>" target="_blank" data-action="share/whatsapp/share">	    
                 <svg class="icon icon-whatsapp">
                     <use xlink:href="#icon-whatsapp"></use>
                 </svg>
