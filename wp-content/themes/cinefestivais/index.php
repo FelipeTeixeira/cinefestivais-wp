b4cbc0cecea96d80822da19dd8134a94ba40abc8
Felipe <felipe.teixeira@gsw.com.br>
2019-09-13 16:50:47 -0300

    area nobre

--- wp-content/themes/cinefestivais/index.php
+++ wp-content/themes/cinefestivais/index.php
@@ -2,10 +2,11 @@
     get_header();
     include 'templates/home/slider.php';
     include 'templates/home/destaques.php';
-    include 'templates/home/especiais.php';
+    include 'templates/home/area-nobre.php';
     include 'templates/home/noticias.php';
     include 'templates/newsletter.php';
-    include 'templates/coberturas.php';
+    include 'templates/coberturas.php';    
+    include 'templates/home/especiais.php';
 
     wp_reset_postdata();    
     get_footer(); 
---
+++ wp-content/themes/cinefestivais/templates/home/area-nobre.php
@@ -0,0 +1,55 @@
+<?php 
+    $argsDestaques = array( 'category_name' => 'area-nobre', 'posts_per_page' => 2);
+    query_posts($argsDestaques);	
+?>  
+<section class="content full articles">
+
+    <?php                    
+        if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+        $count++;
+        $first_name = get_the_author_meta('first_name');
+	    $last_name = get_the_author_meta('last_name');
+    ?>  
+
+        <a href="<?php the_permalink(); ?>" class="article <?php echo ($count === 1) ? "bg-grey" : "bg-blue"; ?>">
+            <?php
+                the_post_thumbnail( 'full', array('title' => get_the_title(), 'alt' => get_the_title(), 'class' => 'is-tablet' ) ); 
+            ?> 
+            <?php if( get_field('image_mobile') ): ?>
+                <img src="<?php the_field('image_mobile'); ?>" class="is-mobile"/>
+            <?php endif; ?>
+            <div class="article-body">
+                <h3 class="article-body__title">
+                    <?php 
+                        $categories = get_the_category();
+                        foreach( $categories as $category) 
+                        {
+                            $name = $category->name;
+                            $slug = $category->slug;                                     
+
+                            if ($slug !== 'area-nobre') 
+                            {
+                                echo esc_attr( $name);                    
+                            }
+                        }
+                    ?>
+                </h3>
+                <h4 class="article-body__subtitle">
+                    <?php the_title(); ?>
+                </h4>
+
+                <p class="article-body__text">
+                    <?= get_the_excerpt(); ?>
+                </p>
+                <p class="article-body__author">
+                    Por
+                    <?= $first_name ?>
+				    <?= $last_name ?>
+                </p>
+            </div>
+        </a>
+    <?php 
+        endwhile; 
+        endif; 
+    ?>    
+</section>
