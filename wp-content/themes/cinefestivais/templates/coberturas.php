d322b315dbd4da9a1114690c6c303be9b5933c8b
Felipe <felipe.teixeira@gsw.com.br>
2019-09-15 13:20:24 -0300

    link para pagina de cobertura correto

--- wp-content/themes/cinefestivais/templates/coberturas.php
+++ wp-content/themes/cinefestivais/templates/coberturas.php
@@ -1,11 +1,14 @@
+<?php
+    $page_coberturas	= esc_url( home_url( '/' ) )."coberturas";
+?>
 <section class="content coberturas-container">
 
     <?php
         include get_template_directory().'/templates/list-coberturas.php';
     ?>
 
-    <a href="<?= $menu_coberturas ?>" class="btn btn-primary btn-centerPage">
-        Ver Todos
+    <a href="<?= $page_coberturas ?>" class="btn btn-primary btn-centerPage">
+        Ver Todas
     </a>
 
 </section>
