6247de1d7f7eec7dff5323aaaea5a761816a9093
Felipe <felipe.teixeira@gsw.com.br>
2019-09-15 13:22:37 -0300

    bloco de especiais

--- wp-content/themes/cinefestivais/templates/home/especiais.php
+++ wp-content/themes/cinefestivais/templates/home/especiais.php
@@ -1,55 +1,22 @@
 <?php 
-    $argsDestaques = array( 'category_name' => 'especiais', 'posts_per_page' => 2);
-    query_posts($argsDestaques);	
+    $argsEspeciais = array( 'category_name' => 'especiais', 'posts_per_page' => 6);
+    query_posts($argsEspeciais);
+
+    $page_especiais	= esc_url( home_url( '/' ) )."especiais";
 ?>  
-<section class="content full articles">
 
-    <?php                    
-        if ( have_posts() ) : while ( have_posts() ) : the_post(); 
-        $count++;
-        $first_name = get_the_author_meta('first_name');
-	    $last_name = get_the_author_meta('last_name');
-    ?>  
+<section class="content">
+    <h2 class="content-title">
+        Especiais
+    </h2>
 
-        <a href="<?php the_permalink(); ?>" class="article <?php echo ($count === 1) ? "bg-grey" : "bg-blue"; ?>">
-            <?php
-                the_post_thumbnail( 'full', array('title' => get_the_title(), 'alt' => get_the_title(), 'class' => 'is-tablet' ) ); 
-            ?> 
-            <?php if( get_field('image_mobile') ): ?>
-                <img src="<?php the_field('image_mobile'); ?>" class="is-mobile"/>
-            <?php endif; ?>
-            <div class="article-body">
-                <h3 class="article-body__title">
-                    <?php 
-                        $categories = get_the_category();
-                        foreach( $categories as $category) 
-                        {
-                            $name = $category->name;
-                            $slug = $category->slug;                                     
-
-                            if ($slug !== 'especiais') 
-                            {
-                                echo esc_attr( $name);                    
-                            }
-                        }
-                    ?>
-                </h3>
-                <h4 class="article-body__subtitle">
-                    <?php the_title(); ?>
-                </h4>
-
-                <p class="article-body__text">
-                    <?= get_the_excerpt(); ?>
-                </p>
-                <p class="article-body__author">
-                    Por
-                    <?= $first_name ?>
-				    <?= $last_name ?>
-                </p>
-            </div>
-        </a>
-    <?php 
-        endwhile; 
-        endif; 
+    <?php
+		$ignoreCategory = 'especiais';
+		include get_template_directory().'/templates/list-card.php';
     ?>    
+    
+    <a href="<?= $page_especiais ?>" class="btn btn-primary btn-centerPage p-margin-bottom-32">
+        Ver Todas
+    </a>
+    
 </section>
