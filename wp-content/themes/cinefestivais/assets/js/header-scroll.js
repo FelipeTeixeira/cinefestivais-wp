357d3beb4b0adce7feaa7e801f9712cae2d1a52e
Felipe <felipe.teixeira@gsw.com.br>
2019-10-07 10:15:40 -0300

    correcao bug scroll

--- wp-content/themes/cinefestivais/assets/js/header-scroll.js
+++ wp-content/themes/cinefestivais/assets/js/header-scroll.js
@@ -7,52 +7,51 @@ var eleHeader = null;
 
 
 const classes = {
-  pinned: 'header-pin',
-  unpinned: 'header-unpin',
+	pinned: 'header-pin',
+	unpinned: 'header-unpin',
 };
 
 
 function onScroll() {
-  currentScrollY = window.pageYOffset;
-  requestTick();
+	currentScrollY = Math.min(Math.max(window.pageYOffset, 0), document.body.offsetHeight - window.innerHeight)
+	requestTick();
 }
 
-
 function requestTick() {
-  if (!ticking) {
-    requestAnimationFrame(update);
-  }
-  ticking = true;
+	if (!ticking) {
+		requestAnimationFrame(update);
+	}
+	ticking = true;
 }
 
 function update() {
-  if (currentScrollY < lastKnownScrollY) {
-    pin();
-  } else if (currentScrollY > lastKnownScrollY) {
-    unpin();
-  }
-  lastKnownScrollY = currentScrollY;
-  ticking = false;
+	if (currentScrollY < lastKnownScrollY) {
+		pin();
+	} else if (currentScrollY > lastKnownScrollY) {
+		unpin();
+	}
+	lastKnownScrollY = currentScrollY;
+	ticking = false;
 }
 
 
 function pin() {
-  if (eleHeader.classList.contains(classes.unpinned)) {
-    eleHeader.classList.remove(classes.unpinned);
-    eleHeader.classList.add(classes.pinned);
-  }
+	if (eleHeader.classList.contains(classes.unpinned)) {
+		eleHeader.classList.remove(classes.unpinned);
+		eleHeader.classList.add(classes.pinned);
+	}
 }
 
 
 function unpin() {
-  if (eleHeader.classList.contains(classes.pinned) || !eleHeader.classList.contains(classes.unpinned)) {
-    eleHeader.classList.remove(classes.pinned);
-    eleHeader.classList.add(classes.unpinned);
-  }
+	if (eleHeader.classList.contains(classes.pinned) || !eleHeader.classList.contains(classes.unpinned)) {
+		eleHeader.classList.remove(classes.pinned);
+		eleHeader.classList.add(classes.unpinned);
+	}
 }
 
-window.onload = function(){
-  eleHeader = document.getElementById(idOfHeader);
-  document.addEventListener('scroll', onScroll, false);
+window.onload = function () {
+	eleHeader = document.getElementById(idOfHeader);
+	document.addEventListener('scroll', onScroll, false);
 }
 
