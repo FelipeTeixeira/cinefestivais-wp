cbd6c8e7cadcacd48158d5f2c79367e52d1abaab
Felipe <felipe.teixeira@gsw.com.br>
2019-10-25 15:55:14 -0300

    versao css

--- wp-content/themes/cinefestivais/header.php
+++ wp-content/themes/cinefestivais/header.php
@@ -26,7 +26,7 @@
         <?php get_title(); ?>
     </title>
 
-    <link rel="stylesheet" href="<?= $url; ?>/assets/css/style.css?v=17">
+    <link rel="stylesheet" href="<?= $url; ?>/assets/css/style.css?v=18">
 
     <?php
         if ($pageClass === 'homePg') {
